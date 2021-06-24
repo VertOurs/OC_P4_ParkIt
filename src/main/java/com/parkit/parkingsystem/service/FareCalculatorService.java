9a193bf1cfebcc14767d49d8119c81263e1639a2
VertOurs <vertour.dev@gmail.com>
2021-06-24 16:14:23 +0200

    git add

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -4,14 +4,21 @@ import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 import java.time.*;
 import java.util.Date;
+import java.sql.*;
 
 
 public class FareCalculatorService {
 
     private LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
+            ZoneId a = ZoneId.systemDefault();
+
         return dateToConvert.toInstant()
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime();
+    }
+
+    public static double calculatePercentage(double total, double remise) {
+        return total *  remise/100;
     }
 
     public void calculateFare(Ticket ticket){
@@ -19,21 +26,33 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        LocalDateTime inHour = convertToLocalDateTimeViaInstant(ticket.getInTime());
-        LocalDateTime outHour = convertToLocalDateTimeViaInstant(ticket.getOutTime());
+            LocalDateTime inHour = convertToLocalDateTimeViaInstant(ticket.getInTime());
+            LocalDateTime outHour = convertToLocalDateTimeViaInstant(ticket.getOutTime());
+            long duration = Duration.between(inHour, outHour).toMinutes();
+            long durationThirtyMinutes = duration;
 
+           // features  free 30 minutes--------------------------------------------------------
+            if(duration < 30) {
+                duration = 0;
+            }
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
+            //features  5%-----------------------------------------------------------------------
+            long carRateWithDiscount = (long) Fare.CAR_RATE_PER_HOUR;
+            long bikeRateWithDiscount = (long) Fare.BIKE_RATE_PER_HOUR;
 
-        long duration = Duration.between(inHour, outHour).toMinutes();
-
+             //if(){
+        carRateWithDiscount = (long) (Fare.CAR_RATE_PER_HOUR - calculatePercentage(Fare.CAR_RATE_PER_HOUR, Fare.DISCOUNT_FOR_OLD_CUSTOMER));
+        bikeRateWithDiscount = (long) (Fare.BIKE_RATE_PER_HOUR - calculatePercentage(Fare.BIKE_RATE_PER_HOUR, Fare.DISCOUNT_FOR_OLD_CUSTOMER));
+               // }
+            // le switch
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR)/60);
+                ticket.setPrice((durationThirtyMinutes * carRateWithDiscount)/60);
+
                 break;
             }
             case BIKE: {
-                ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR)/60);
+                ticket.setPrice((durationThirtyMinutes * bikeRateWithDiscount)/60);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
